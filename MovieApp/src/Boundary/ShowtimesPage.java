61524df95aff25407afbf1fd04720e1cea1adc7c
AdeebHossain <adeebhossain2004@gmail.com>
2024-12-01 20:27:30 -0700

    spaciing

--- MovieApp/src/Boundary/ShowtimesPage.java
+++ MovieApp/src/Boundary/ShowtimesPage.java
@@ -17,7 +17,7 @@ public class ShowtimesPage {
     private static final SeatingController seatingController = new SeatingController();
 
     public static void show(String movieTitle, String[] showtimes, String movieDescription, ImageIcon movieImage, int movieLength) {
-        JFrame frame = new JFrame("Showtimes - " + movieTitle + " " + movieLength);
+        JFrame frame = new JFrame("Showtimes - " + movieTitle + " (" + movieLength + " m)");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
